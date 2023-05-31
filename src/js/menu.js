14e57cff976563281780eeec3ed6622229f67172
PolinaKrasovskaya <polina.vorotnik@gmail.com>
2023-06-01 00:06:01 +0100

    mobile menu

--- src/js/menu.js
+++ src/js/menu.js
@@ -2,6 +2,7 @@
 (() => {
   const menuBtnRef = document.querySelector("[data-menu-button]");
   const mobileMenuRef = document.querySelector("[data-menu]");
+  const mobileMenuLink = document.querySelector("[data-link]");
 
   menuBtnRef.addEventListener("click", () => {
     const expanded =
@@ -11,5 +12,20 @@
     menuBtnRef.setAttribute("aria-expanded", !expanded);
 
     mobileMenuRef.classList.toggle("is-open");
+    // mobileMenuRef.setAttribute("aria-expanded", !expanded);
   });
+
+  mobileMenuLink.addEventListener("click", () => {
+    console.log("link click")
+    const isOpen = mobileMenuRef.getAttribute("is-open") === "true" || false;
+    console.log(isOpen)
+    mobileMenuRef.classList.toggle("is-open");
+    mobileMenuRef.setAttribute("is-open", !isOpen);
+
+    const expanded =
+      menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+
+    menuBtnRef.classList.toggle("is-open");
+    menuBtnRef.setAttribute("aria-expanded", !expanded);
+  })
 })();
